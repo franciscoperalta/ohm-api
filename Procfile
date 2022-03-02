ea3fd1b8dc9f30b6f3ed50718b0495a6bcad0055
Francisco Peralta <francisco.peraltarosas@gmail.com>
2022-03-02 00:52:36 -0600

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ohm:app --log-file -
---
+++ ohm.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/potencial/<string:corriente>&<string:resistencia>')
+def getPotencial(corriente, resistencia):
+	potencial = float(corriente) * float(resistencia)
+	return jsonify({"potencial":potencial})
+
+@app.route('/corriente/<string:potencial>&<string:resistencia>')
+def getCorriente(potencial, resistencia):
+	corriente = float(potencial) / float(resistencia)
+	return jsonify({"corriente":corriente})
+
+@app.route('/resistencia/<string:potencial>&<string:corriente>')
+def getResistencia(potencial, corriente):
+	resistencia = float(potencial) / float(corriente)
+	return jsonify({"resistencia":resistencia})
+
+if __name__ == '__main__':
+	app.run(debug=True)
